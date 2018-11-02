b9e97f630278214f7ce67d353158d6c5afd76d5a
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2018-11-02 12:36:08 -0400

    add sprites

---
+++ map/assets/Sprites.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Sprites" tilewidth="70" tileheight="70" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image width="33" height="70" source="../../Images/terrorists/3/idle/3_terrorist_3_Idle_000.png"/>
+ </tile>
+ <tile id="9">
+  <image width="40" height="70" source="../../Images/terrorists/3/jump/3_terrorist_3_Jump_000.png"/>
+ </tile>
+ <tile id="10">
+  <image width="70" height="58" source="../../Images/terrorists/2/Attack4/2_terrorist_2_Attack4_000.png"/>
+ </tile>
+ <tile id="11">
+  <image width="37" height="70" source="../../Images/terrorists/1/run/1_terrorist_1_Run_003.png"/>
+ </tile>
+ <tile id="12">
+  <image width="37" height="70" source="../../Images/terrorists/1/run/1_terrorist_1_Run_003 - Copy (2).png"/>
+ </tile>
+</tileset>
