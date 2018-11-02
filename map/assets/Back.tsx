b4d49c4ee05ff1dfda16736b1b2121b3e6d97c69
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2018-11-02 12:13:54 -0400

    add map

---
+++ map/assets/Back.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Back" tilewidth="70" tileheight="70" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="70" height="70" source="back.png"/>
+ </tile>
+ <tile id="2">
+  <image width="70" height="70" source="land.png"/>
+ </tile>
+ <tile id="3">
+  <image width="70" height="70" source="sky.png"/>
+ </tile>
+ <tile id="4">
+  <image width="70" height="70" source="sky corner.png"/>
+ </tile>
+ <tile id="5">
+  <image width="70" height="70" source="sky land.png"/>
+ </tile>
+</tileset>
