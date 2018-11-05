e9696d592106fa780742e29c0c1aed96cdce0177
dpola788 <dpola788@students.bju.edu>
2018-11-04 23:52:46 -0500

    Added High Score files

---
+++ Model/HighScores/HighScoresManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Royale_Platformer.Model.HighScores
+{
+    class HighScoresManager
+    {
+        private List<string> playerNames;
+        private List<int> playerScores;
+        public HighScoresManager()
+        {
+            playerNames = new List<string>();
+            playerScores = new List<int>();
+        }
+
+        // Adds a player's name <name> and score <score> to the dictionary
+        public void AddScore(string playerName, int playerScore)
+        {
+            playerNames.Add(playerName);
+            playerScores.Add(playerScore);
+        }
+        
+        // Writes the highScores Dictionary to a file
+        public void WriteScores()
+        {
+
+        }
+
+        // Reads names and scores from a file and assigns them to the highScores Dictionary
+        public void ReadScores()
+        {
+
+        }
+
+        public List<string> GetNames()
+        {
+            return playerNames;
+        }
+
+        public List<int> GetScores()
+        {
+            return playerScores;
+        }
+    }
+}
---
+++ Model/HighScores/HighScoresUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Royale_Platformer.Model.HighScores
+{
+    [TestFixture]
+    class HighScoresUnitTests
+    {
+        [Test]
+        public void AddScore_PlayerAndScore_AddsToLists()
+        {
+            List<string> myNames = new List<string>();
+            List<int> myScores = new List<int>();
+            myNames.Add("David");
+            myNames.Add("Matthew");
+            myNames.Add("Stephen");
+            myScores.Add(100);
+            myScores.Add(200);
+            myScores.Add(300);
+
+            HighScoresManager h = new HighScoresManager();
+            h.AddScore("David", 100);
+            h.AddScore("Matthew", 200);
+            h.AddScore("Stephen", 300);
+            List<string> names = h.GetNames();
+            List<int> scores = h.GetScores();
+            for (int i = 0; i < myNames.Count; i++)
+            {
+                Assert.IsTrue(myNames[i] == names[i]);
+            }
+
+            for (int i = 0; i < myScores.Count; i++)
+            {
+                Assert.IsTrue(myScores[i] == scores[i]);
+            }
+        }
+    }
+}
