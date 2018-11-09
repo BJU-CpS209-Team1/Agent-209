b6542050979c4e73e3958d0e8eddb74bf3a94852
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2018-11-09 14:30:39 -0500

    add special forces characters

---
+++ Assets/map/assets/Deluxe Background.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Deluxe Background" tilewidth="1024" tileheight="512" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1024" height="512" source="platformer-art-complete-pack-0/Backgrounds/bg_desert.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1024" height="512" source="platformer-art-complete-pack-0/Backgrounds/bg_grasslands.png"/>
+ </tile>
+ <tile id="2">
+  <image width="1024" height="512" source="platformer-art-complete-pack-0/Backgrounds/bg_shroom.png"/>
+ </tile>
+ <tile id="3">
+  <image width="1024" height="512" source="platformer-art-complete-pack-0/Backgrounds/bg_castle.png"/>
+ </tile>
+</tileset>
