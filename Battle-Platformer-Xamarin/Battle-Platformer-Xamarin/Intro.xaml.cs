d7bcba408b718ecd00126b7f6d2f6fefa522e9c7
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2018-11-19 17:09:25 -0500

    add video intro

---
+++ Battle-Platformer-Xamarin/Battle-Platformer-Xamarin/Intro.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Battle_Platformer_Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Intro : ContentPage
+    {
+        bool continueGame;
+        bool hardcore;
+
+        public Intro(bool continueGame, bool hardcore)
+        {
+            InitializeComponent();
+            this.continueGame = continueGame;
+            this.hardcore = hardcore;
+
+            // End of video
+            Device.StartTimer(TimeSpan.FromMilliseconds(23500), () =>
+            {
+                App.Current.MainPage = new Game(continueGame, hardcore);
+                return false;
+            });
+        }
+    }
+}
