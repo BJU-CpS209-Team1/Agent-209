2e08d74a1649faba0d9c4e986e22d19c6fa6fcc2
IsaacAbrahamson <isaac.abrahamson@programmer.net>
2018-11-27 14:59:07 -0500

    basic shotgun

--- Battle-Platformer-Xamarin/Battle-Platformer-Xamarin/Model/WeaponShotgun.cs
+++ Battle-Platformer-Xamarin/Battle-Platformer-Xamarin/Model/WeaponShotgun.cs
@@ -1,13 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Urho;
 
 namespace Royale_Platformer.Model
 {
     public class WeaponShotgun : Weapon
     {
+        public WeaponShotgun()
+        {
+            Cooldown = 20;
+        }
+
         public override List<Bullet> Fire(Vector2 dir)
         {
-            throw new System.NotImplementedException();
+            List<Bullet> bl = new List<Bullet>();
+            for (int i = 0; i < 10; ++i)
+            {
+
+                Bullet b = new Bullet(1);
+
+                Random gen = new Random();
+                float randX = (float) (gen.NextDouble() * (0.3 - 0) + 0);
+                float randY = (float) (gen.NextDouble() * (0.3 - 0) + 0);
+                dir.X = dir.X + randX;
+                dir.Y = dir.Y + randY;
+
+                b.Direction = dir;
+                bl.Add(b);
+            }
+            return bl;
         }
 
 
